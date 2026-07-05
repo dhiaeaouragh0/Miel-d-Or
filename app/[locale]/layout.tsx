76de8f67602e368220bcdb715c114487cc78de71
dhiaeaouragh0 <dhiaeaouragh0@gmail.com>
2026-07-05 20:31:24 +0100

    remove hasLocale

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Inter, Cairo } from "next/font/google";
-import { NextIntlClientProvider, hasLocale } from "next-intl";
+import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { notFound } from "next/navigation";
-import "../globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { AnnouncementBar } from "@/components/AnnouncementBar";
--- i18n/request.ts
+++ i18n/request.ts
@@ -1,5 +1,4 @@
 import { getRequestConfig } from "next-intl/server";
-import { hasLocale } from "next-intl";
 import { routing } from "./routing";
 
 export default getRequestConfig(async ({ requestLocale }) => {
